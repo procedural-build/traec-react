c958360bed0791cb8ffaab2fb106b3df94ce7ee7
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-06-21 09:51:11 +0200

    updated test for footer

--- test/footer/index.test.js
+++ test/footer/index.test.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { TraecFooter } from "../../src/footer";
+import TestRenderer from "react-test-renderer";
+jest.mock("react-router-dom", () => ({ Link: "Link" }));
 
 describe("Footer", () => {
-  let wrapper;
-  beforeEach(() => (wrapper = shallow(<TraecFooter />)));
-
-  it("should render three <div />s", () => {
-    expect(wrapper.find("div").length).toEqual(6);
+  it("should render three <Link />s", () => {
+    const wrapper = shallow(<TraecFooter />);
+    expect(wrapper.find("Link").length).toEqual(3);
+    expect(wrapper.find("Link").get(0).props.to).toEqual("/news");
+    expect(wrapper.find("Link").get(1).props.to).toEqual("/about");
+    expect(wrapper.find("Link").get(2).props.to).toEqual("/help");
   });
 
   it("should match snapshot", () => {
-    expect(wrapper).toMatchSnapshot();
+    const testRenderer = TestRenderer.create(<TraecFooter />);
+    expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
