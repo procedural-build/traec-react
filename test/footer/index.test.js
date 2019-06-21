95e77020dc93b7d5f13d78c55cdfc5111ba680c9
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-06-21 09:25:26 +0200

    bug fixes to tests

--- test/footer/index.test.js
+++ test/footer/index.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { Footer } from "../../src/footer";
+import { TraecFooter } from "../../src/footer";
 
 describe("Footer", () => {
   let wrapper;
-  beforeEach(() => (wrapper = shallow(<Footer />)));
+  beforeEach(() => (wrapper = shallow(<TraecFooter />)));
 
   it("should render three <div />s", () => {
-    expect(wrapper.find("div").length).toEqual(3);
+    expect(wrapper.find("div").length).toEqual(6);
   });
 
   it("should match snapshot", () => {
