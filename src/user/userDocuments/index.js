689168b493ecfd36dce6cd98129be3844fa926d8
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-10-10 14:40:38 +0200

    finished my documents list

--- src/user/userDocuments/index.js
+++ src/user/userDocuments/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BSCard } from "traec-react/utils/bootstrap";
 import { connect } from "react-redux";
 import Traec from "traec";
-import Im from "traec/immutable";
 import { UserDocumentItem } from "./documentItem";
+import { Spinner } from "traec-react/utils/entities";
 
 class Index extends React.Component {
   constructor(props) {
@@ -36,8 +36,8 @@ class Index extends React.Component {
   renderDocuments() {
     let { documents } = this.props;
 
-    if (!documents) {
-      return null;
+    if (!documents || documents.length === 0) {
+      return <Spinner explanation="Loading Documents" timedOutComment="No Documents Found" />;
     }
 
     return documents.map((document, index) => <UserDocumentItem key={index} document={document} index={index} />);
@@ -54,13 +54,25 @@ export const getDocumentsFromState = function(state) {
     documents = userDocuments.toArray().map(document => {
       let descriptionId = document[1].get("description");
       let statusId = document[1].get("status");
+      let trackerId = document[1].get("trackerId");
+      let { project, company } = getCompanyProjectFromTracker(state, trackerId);
       return {
         title: state.getInPath(`entities.descriptions.byId.${descriptionId}.title`),
-        status: state.getInPath(`entities.docStatus.byId.${statusId}.status`)
+        status: state.getInPath(`entities.docStatus.byId.${statusId}.status`),
+        project,
+        company
       };
     });
   }
   return documents;
+};
+
+export const getCompanyProjectFromTracker = function(state, trackerId) {
+  let projectId = state.getInPath(`entities.trackers.byId.${trackerId}.project.uid`);
+  let project = state.getInPath(`entities.projects.byId.${projectId}`);
+  let companyName = state.getInPath(`entities.projects.byId.${projectId}.company.name`);
+
+  return { project, company: companyName };
 };
 
 const mapStateToProps = state => {
