2523583743ae173166c314b24710ab7e2b827466
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-12-18 15:22:06 +0100

    reverted matias changes

--- test/user/userDocuments/userDocuments.test.js
+++ test/user/userDocuments/userDocuments.test.js
@@ -4,7 +4,10 @@ import { getDocumentsFromState } from "../../../src/user/userDocuments";
 describe("get documents from state", () => {
   it("Should give back the documents", () => {
     let actualResult = getDocumentsFromState(documentsState);
-    let expectedResult = [{ title: "Doc 1", status: null }, { title: "Doc 2", status: null }];
+    let expectedResult = [
+      { title: "Doc 1", status: null },
+      { title: "Doc 2", status: null }
+    ];
     expect(actualResult).toEqual(expectedResult);
   });
 });
