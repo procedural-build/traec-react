b98fdbbde2b7e50ade4ff9f0e73d50c5fd136a15
MatiasStorm <matias.storm42@gmail.com>
2020-01-27 15:23:01 +0100

    Added test for TitleAndDescription component

---
+++ test/document/titleAndDescription.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { TitleAndDescription } from "traec-react/documents/titleAndDescription";
+import Im from "traec/immutable";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+const data = Im.fromJS({
+  title: "I am a title for the TitleDescription component",
+  text: "I am text for the titleDescription component"
+});
+describe("TitleAndDescription cards", () => {
+  it("should render the card", () => {
+    const wrapper = shallow(<TitleAndDescription description={data} />);
+    expect(wrapper.find("h2").text()).toEqual("I am a title for the TitleDescription component");
+  });
+});
