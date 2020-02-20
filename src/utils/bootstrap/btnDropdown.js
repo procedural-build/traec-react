9b43a7db66df51e2261860ebf8777c6c2ba05230
Christian Kongsgaard <kongsgaard.c@gmail.com>
2020-02-20 09:16:49 +0100

    removed margin to dropdown

--- src/utils/bootstrap/btnDropdown.js
+++ src/utils/bootstrap/btnDropdown.js
@@ -79,8 +79,8 @@ export class BSBtnDropdown extends React.Component {
     }
     return (
       <div
-        className="dropdown-menu dropdown-menu-right mt-5"
-        style={{ maxHeight: "50rem", overflowX: "scroll" }}
+        className="dropdown-menu dropdown-menu-right"
+        style={{ maxHeight: "40rem", overflowX: "scroll" }}
         aria-labelledby="btnDropdown"
       >
         {links}
