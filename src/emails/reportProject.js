1d1e37f4a5d274e1ec4057b8c978bd00960dca48
Christian Kongsgaard <kongsgaard.c@gmail.com>
2020-05-07 15:30:14 +0200

    updated email charts

--- src/emails/reportProject.js
+++ src/emails/reportProject.js
@@ -3,65 +3,7 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Traec from "traec";
 import { loading } from "traec-react/utils/entities";
-import ReportBarPlot from "traec-react/emails/reportBarChart";
-
-export const EMAIL_TYPES = [
-  "project_invite",
-  "project_ref_near_due",
-  "project_ref_overdue",
-  "project_commit_committed",
-  "project_commit_rejected",
-  "project_commit_approved"
-];
-
-export const EMAIL_TYPE_HEADERS = ["Invite", "Near Due", "Overdue", "Submitted", "Rejected", "Approved"];
-
-function ProjectEmailRecipient({ recipient, projectId }) {
-  let total = 0;
-
-  let cols = EMAIL_TYPES.map((emailType, i) => {
-    let num = recipient.getInPath(`sent_summary.email_types.${emailType}`);
-    total += parseInt(num) || 0;
-    return (
-      <div key={i} className="col-sm-1 text-center">
-        {num}
-      </div>
-    );
-  });
-
-  // Add the total to the last column
-  cols.push(
-    <div key={cols.length} className="col-sm-1 text-center">
-      {total}
-    </div>
-  );
-
-  return (
-    <div className="row" style={{ borderTop: "1px solid grey" }}>
-      <div className="col-sm-5">
-        <Link to={`/project/${projectId}/email/report/${recipient.get("uid")}`}>{recipient.get("email")}</Link>
-      </div>
-      {cols}
-    </div>
-  );
-}
-
-function RecipientTableHeaders() {
-  let cols = EMAIL_TYPE_HEADERS.concat(["Total"]).map((header, i) => (
-    <div key={i} className="col-sm-1 text-center">
-      <b>{header}</b>
-    </div>
-  ));
-
-  return (
-    <div className="row">
-      <div className="col-sm-5">
-        <b>Email</b>
-      </div>
-      {cols}
-    </div>
-  );
-}
+import { EmailBarChart } from "traec-react/emails/emailBarChart";
 
 class ProjectEmailReport extends React.Component {
   constructor(props) {
@@ -85,63 +27,51 @@ class ProjectEmailReport extends React.Component {
 
   componentDidUpdate() {
     Traec.fetchRequired.bind(this)();
-    this.setIsLoading();
   }
 
   displayEmpty() {
-    return (
-      <div>
-        <h3>No emails have been sent to any recipients.</h3>
-        <h5>Ensure there are members associated to this project first</h5>
-      </div>
-    );
-  }
-
-  displayEmailData({ recipients, emails }) {
-    return (
-      <div>
-        <h2>Email Recipients</h2>
-
-        <p>The following email addresses have received notifications on this project.</p>
-        <div className="emailPlot">{recipients ? <ReportBarPlot emails={emails} /> : null} </div>
-        <RecipientTableHeaders />
-      </div>
-    );
-  }
-
-  setIsLoading() {
-    let { recipients, emails } = this.props;
-    if (!recipients && !emails) {
+    if (!this.props.recipients && !this.props.emails) {
+      return loading("report");
+    } else if (!this.props.recipients.size && !this.props.emails.size) {
+      return (
+        <div>
+          <h3>No emails have been sent to any recipients.</h3>
+          <h5>Ensure there are members associated to this project first</h5>
+        </div>
+      );
+    } else {
       return null;
-    } else if (recipients && this.state.isLoading) {
-      this.setState({ isLoading: false });
     }
   }
 
   render() {
-    let { recipients, projectId, emails } = this.props;
+    let { recipients, projectId } = this.props;
     if (!recipients) {
       return null;
     }
 
-    let rows = null;
-
-    rows = recipients
+    let rows = recipients
       .toList()
+      .sortBy(recipient => recipient.get("email").toLowerCase())
       .map((recipient, i) => <ProjectEmailRecipient key={i} recipient={recipient} projectId={projectId} />);
+
+    if (!rows || !rows.size) {
+      rows = <div>No email data found.</div>;
+    }
 
     return (
       <React.Fragment>
-        {this.state.isLoading ? loading("report") : null}
-
-        {(!this.state.isLoading && recipients.toList().size == 0) || !emails
-          ? this.displayEmpty()
-          : (!this.state.isLoading && emails.toList().size == 0) || !emails
-          ? this.displayEmpty()
-          : !this.state.isLoading && recipients.toList().size !== 0 && emails
-          ? this.displayEmailData({ recipients, emails })
-          : null}
-        {rows}
+        {this.displayEmpty()}
+        <EmailBarChart
+          emails={this.props.emails}
+          title={"Email Recipients"}
+          text={"The following email addresses have received notifications on this project."}
+        />
+        <div className="m-3 p-3">
+          <RecipientTableHeaders />
+          <hr className="m-1 p-0" />
+          {rows}
+        </div>
       </React.Fragment>
     );
   }
@@ -150,9 +80,11 @@ class ProjectEmailReport extends React.Component {
 const mapStateToProps = (state, ownProps) => {
   let { projectId } = Traec.utils.getFullIds(state, ownProps.match.params);
 
+  let recipients = null;
+  let emails = null;
   if (projectId) {
-    var recipients = state.getInPath(`entities.projectObjects.byId.${projectId}.recipients`);
-    var emails = state.getInPath(`entities.projectObjects.byId.${projectId}.emails`);
+    recipients = state.getInPath(`entities.projectObjects.byId.${projectId}.recipients`);
+    emails = state.getInPath(`entities.projectObjects.byId.${projectId}.emails`);
   }
 
   // Add this to props
@@ -164,3 +96,68 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(mapStateToProps)(ProjectEmailReport);
+
+export const EMAIL_TYPES = [
+  "project_invite",
+  "project_ref_near_due",
+  "project_ref_overdue",
+  "project_ref_submitted",
+  "project_ref_rejected",
+  "project_ref_approved"
+];
+
+export const EMAIL_TYPE_HEADERS = ["Invite", "Near Due", "Overdue", "Submitted", "Rejected", "Approved"];
+
+const ProjectEmailRecipient = props => {
+  let { recipient, projectId } = props;
+  let total = 0;
+
+  let cols = EMAIL_TYPES.map((emailType, i) => {
+    //debugger;
+    let num = recipient.getInPath(`sent_summary.email_types.${emailType}`) || 0;
+    total += parseInt(num) || 0;
+    return (
+      <div key={i} className="col-sm-1 text-center">
+        {num}
+      </div>
+    );
+  });
+
+  // Add the total to the last column
+  cols.push(
+    <div key={cols.length} className="col-sm-1 text-center">
+      {total}
+    </div>
+  );
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm-5">
+          <Link to={`/project/${projectId}/email/report/${recipient.get("uid")}`}>
+            {recipient.get("email").toLowerCase()}
+          </Link>
+        </div>
+        {cols}
+      </div>
+      <hr className="m-1 p-0" />
+    </div>
+  );
+};
+
+const RecipientTableHeaders = () => {
+  let cols = EMAIL_TYPE_HEADERS.concat(["Total"]).map((header, i) => (
+    <div key={i} className="col-sm-1 text-center">
+      <b>{header}</b>
+    </div>
+  ));
+
+  return (
+    <div className="row">
+      <div className="col-sm-5">
+        <b>Email</b>
+      </div>
+      {cols}
+    </div>
+  );
+};
--- src/emails/reportRecipientProject.js
+++ src/emails/reportRecipientProject.js
@@ -1,36 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Traec from "traec";
+import Im from "traec/immutable";
 import { loading } from "traec-react/utils/entities";
-import Moment, { min } from "moment";
-import IndividualReportBarPlot from "traec-react/emails/reportRecipientBarChart";
-
-function ProjectSentEmail({ item, recipient, projectId }) {
-  return (
-    <div className="row" style={{ borderTop: "1px solid grey" }}>
-      <div className="col-sm-6">{Moment(item.get("date")).format("MMM Do YY, h:mm:ss a")}</div>
-      <div className="col-sm-3">{item.get("email_type")}</div>
-      <div className="col-sm-3">{item.get("extra_info")}</div>
-    </div>
-  );
-}
-
-function RecipientTableHeaders() {
-  return (
-    <div className="row">
-      <div className="col-sm-6">
-        <b>Date</b>
-      </div>
-      <div className="col-sm-3">
-        <b>Email Type</b>
-      </div>
-      <div className="col-sm-3">
-        <b>Additional Info</b>
-      </div>
-    </div>
-  );
-}
+import Moment from "moment";
+import { EmailBarChart } from "traec-react/emails/emailBarChart";
 
 class ProjectRecipientEmailReport extends React.Component {
   constructor(props) {
@@ -41,8 +15,6 @@ class ProjectRecipientEmailReport extends React.Component {
     };
 
     this.requiredFetches = [new Traec.Fetch("project_email_recipient", "read")];
-
-    // action bindings
   }
 
   componentDidMount() {
@@ -51,36 +23,20 @@ class ProjectRecipientEmailReport extends React.Component {
 
   componentDidUpdate() {
     Traec.fetchRequired.bind(this)();
-    this.setIsLoading();
   }
+
   displayEmpty() {
-    return (
-      <div>
-        <h3>No emails have been sent to this recipient.</h3>
-        <h5>Ensure there are members associated to this project first</h5>
-      </div>
-    );
-  }
-
-  displayEmailData({ recipient }) {
-    return (
-      <div>
-        <h2>Email Report for: {recipient.get("email")}</h2>
-
-        <p>The following email notifications have been sent to this address.</p>
-        <div>{recipient.get("sent") ? <IndividualReportBarPlot recipient={recipient} /> : null}</div>
-        <RecipientTableHeaders />
-      </div>
-    );
-  }
-
-  setIsLoading() {
-    let { recipient } = this.props;
-    if (!recipient) {
+    if (!this.props.recipient) {
+      return loading("report");
+    } else if (!this.props.recipient.size && this.props.recipient.get("sent")) {
+      return (
+        <div>
+          <h3>No emails have been sent to this recipient.</h3>
+          <h5>Ensure there are members associated to this project first</h5>
+        </div>
+      );
+    } else {
       return null;
-    }
-    if (recipient.get("sent") && this.state.isLoading) {
-      this.setState({ isLoading: false });
     }
   }
 
@@ -99,15 +55,18 @@ class ProjectRecipientEmailReport extends React.Component {
 
     return (
       <React.Fragment>
-        {/* Render the loading spinning wheel */}
-        {this.state.isLoading ? loading("report") : null}
-
-        {!this.state.isLoading && recipient.toList().size == 0
-          ? this.displayEmpty()
-          : !this.state.isLoading && recipient.toList().size !== 0
-          ? this.displayEmailData({ recipient })
-          : null}
-        {rows}
+        {this.displayEmpty()}
+        <EmailBarChart
+          emails={parseRecipientEmails(recipient)}
+          title={`Email Report for: ${recipient.get("email")}`}
+          text={"The following email notifications have been sent to this address."}
+        />
+        <div className="m-3 p-3">
+          <h4>Email History</h4>
+          <RecipientTableHeaders />
+          <hr className="m-1 p-0" />
+          {rows}
+        </div>
       </React.Fragment>
     );
   }
@@ -125,3 +84,72 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(mapStateToProps)(ProjectRecipientEmailReport);
+
+const ProjectSentEmail = props => {
+  let { item } = props;
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm-5">{Moment(item.get("date")).format("LLL")}</div>
+        <div className="col-sm-3">{formatEmailName(item.get("email_type"))}</div>
+        <div className="col-sm-4">{item.get("extra_info")}</div>
+      </div>
+      <hr className="m-1 p-0" />
+    </div>
+  );
+};
+
+const formatEmailName = email => {
+  if (email === "project_invite") {
+    return "Project Invite";
+  } else if (email === "project_ref_approved") {
+    return "Report Approved";
+  } else if (email === "project_ref_near_due") {
+    return "Report Near Due";
+  } else if (email === "project_ref_overdue") {
+    return "Report Over Due";
+  } else if (email === "project_ref_submitted") {
+    return "Report Submitted";
+  } else if (email === "project_ref_rejected") {
+    return "Report Rejected";
+  } else {
+    return email;
+  }
+};
+
+const RecipientTableHeaders = () => {
+  return (
+    <div className="row">
+      <div className="col-sm-5">
+        <b>Date</b>
+      </div>
+      <div className="col-sm-3">
+        <b>Email Type</b>
+      </div>
+      <div className="col-sm-4">
+        <b>Additional Info</b>
+      </div>
+    </div>
+  );
+};
+
+const parseRecipientEmails = recipient => {
+  let result = Im.Map();
+  if (!recipient || !recipient.has("sent")) {
+    return result;
+  }
+
+  recipient.get("sent").map(item => {
+    let date = Moment(item.get("date")).format("YYYY-MM");
+    if (result.has(date)) {
+      result = result.setInPath(
+        `${date}.email_types.${item.get("email_type")}`,
+        (result.getInPath(`${date}.email_types.${item.get("email_type")}`) || 0) + 1
+      );
+    } else {
+      result = result.setInPath(`${date}.email_types.${item.get("email_type")}`, 1);
+      result = result.setInPath(`${date}.min_date`, date);
+    }
+  });
+  return result.valueSeq();
+};
