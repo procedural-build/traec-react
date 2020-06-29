bd1cd49eb2b30960f2950dc3420aa8345e806746
Mark Pitman <mark@procedural.build>
2020-06-29 08:39:32 +0200

    Added Accept on Behalf of for project invites

--- src/members/inviteList/item.js
+++ src/members/inviteList/item.js
@@ -13,8 +13,8 @@ export default class InviteItem extends React.Component {
     const i = this.props.index;
     const item = this.props.item;
 
-    let permissionMethod = this.props.projectId ? projectPermissionRender : companyPermissionRender
-    let permissionId = this.props.projectId || this.props.companyId
+    let permissionMethod = this.props.projectId ? projectPermissionRender : companyPermissionRender;
+    let permissionId = this.props.projectId || this.props.companyId;
 
     return (
       <div className="row" key={i} style={{ backgroundColor: (i + 1) % 2 ? "#ddd" : "" }}>
@@ -33,6 +33,30 @@ export default class InviteItem extends React.Component {
 }
 
 const DropdownActions = props => {
+  const respondInvite = e => {
+    e.preventDefault();
+    let { companyId, projectId } = props;
+    let inviteId = props.item.get("uid");
+
+    let accept = true;
+    let status = accept ? "accepted" : "rejected";
+
+    let fetch = null;
+    if (projectId) {
+      fetch = new Traec.Fetch("project_invite", "put", { projectId, inviteId });
+    } else if (companyId) {
+      fetch = new Traec.Fetch("company_invite", "put", { companyId, inviteId });
+    }
+    // Set the payload and dispatch
+    fetch.updateFetchParams({
+      preFetchHook: body => ({ status }),
+      postSuccessHook: () => {
+        location.reload();
+      }
+    });
+    fetch.dispatch();
+  };
+
   const deleteItem = e => {
     e.preventDefault();
     let { companyId, projectId } = props;
@@ -45,7 +69,10 @@ const DropdownActions = props => {
   };
 
   const dropDownLinks = () => {
-    return [{ name: "Delete", onClick: deleteItem }];
+    return [
+      { name: "Accept on behalf of", onClick: respondInvite },
+      { name: "Delete", onClick: deleteItem }
+    ];
   };
   return (
     <div className="col-sm-1">
