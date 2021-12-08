659679c182d15382ce574aec9fc94490473c16cf
Christian Kongsgaard <christian@procedural.build>
2021-12-08 12:03:21 +0100

    ASS-1361: made navbar project permission aware

--- src/navBar/index.js
+++ src/navBar/index.js
@@ -6,95 +6,114 @@ import { MenuItem } from "./item";
 import { DropDownItem } from "./dropdown";
 import { setNavBarItems } from "traec/redux/actionCreators";
 import { isSuperuser } from "../utils";
+import { getProjectPermissions } from "traec/utils/permissions/project";
 
 export { setNavBarItems };
 
-export class NavBar extends React.Component {
-  renderItems() {
-    if (this.props.items == null) {
-      return null;
-    }
-    return this.props.items.map((item, i) => renderItem(item, i, this.props.user));
-  }
+export const NavBar = props => {
+  let {
+    items,
+    user,
+    brand,
+    extraClass,
+    preUserItems,
+    include_myprofile,
+    includeUser,
+    userLabel,
+    getUserItemLabel,
+    createText,
+    permission
+  } = props;
 
-  renderBrand() {
-    let { brand } = this.props;
-    if (brand) {
-      return brand;
-    }
-    return (
-      <React.Fragment>
-        Traec | <b>Procedural</b>.build
-      </React.Fragment>
-    );
-  }
-
-  render() {
-    let {
-      extraClass,
-      include_myprofile = true,
-      includeUser = false,
-      userLabel = null,
-      getUserItemLabel = null
-    } = this.props;
-    return (
-      <div className="navbar-area">
-        <nav
-          className={`navbar navbar-expand-lg navbar-light bg-light navbar-static-top ${extraClass}`}
-          role="navigation"
-        >
-          <div className="container-fluid m-0 p-0">
-            <div className="navbar-brand">{this.renderBrand()}</div>
-
-            <button
-              className="navbar-toggler border-0"
-              type="button"
-              data-toggle="collapse"
-              data-target="#exCollapsingNavbar"
-            >
-              &#9776;
-            </button>
-
-            <div className="collapse navbar-collapse" id="exCollapsingNavbar">
-              <ul className="nav navbar-nav flex-row justify-content-between ml-auto">
-                {this.renderItems()}
-                {this.props.preUserItems}
-                <DropdownLogin
-                  include_myprofile={include_myprofile}
-                  includeUser={includeUser}
-                  userLabel={userLabel}
-                  getUserItemLabel={getUserItemLabel}
-                  createText={this.props.createText}
-                />
-              </ul>
-            </div>
+  return (
+    <div className="navbar-area">
+      <nav
+        className={`navbar navbar-expand-lg navbar-light bg-light navbar-static-top ${extraClass}`}
+        role="navigation"
+      >
+        <div className="container-fluid m-0 p-0">
+          <div className="navbar-brand">
+            <NavBrand brand={brand} />
           </div>
-        </nav>
-      </div>
-    );
-  }
-}
 
-const renderItem = function(item, keyIndex, user) {
+          <button
+            className="navbar-toggler border-0"
+            type="button"
+            data-toggle="collapse"
+            data-target="#exCollapsingNavbar"
+          >
+            &#9776;
+          </button>
+
+          <div className="collapse navbar-collapse" id="exCollapsingNavbar">
+            <ul className="nav navbar-nav flex-row justify-content-between ml-auto">
+              <NavItems items={items} user={user} permission={permission} />
+              {preUserItems}
+              <DropdownLogin
+                include_myprofile={include_myprofile}
+                includeUser={includeUser}
+                userLabel={userLabel}
+                getUserItemLabel={getUserItemLabel}
+                createText={createText}
+              />
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+const NavItems = props => {
+  if (props.items == null) {
+    return null;
+  }
+  return props.items.map((item, i) => <NavItem key={i} item={item} user={props.user} permission={props.permission} />);
+};
+
+const NavBrand = props => {
+  let { brand } = props;
+  if (brand) {
+    return brand;
+  }
+  return (
+    <>
+      Traec | <b>Procedural</b>.build
+    </>
+  );
+};
+
+const NavItem = props => {
+  const { item, permission, user } = props;
   const label = item.get("label");
   const to = item.get("to");
   const requiresAdmin = item.get("requiresAdmin");
 
-  if (requiresAdmin && !isSuperuser(user)) {
+  if (requiresAdmin && !isSuperuser(user) && !permission?.get("is_admin")) {
     return null;
   }
 
   if (Im.List.isList(to)) {
-    return <DropDownItem key={keyIndex} label={label} items={to} />;
+    return <DropDownItem label={label} items={to} />;
   } else {
-    return <MenuItem key={keyIndex} label={label} to={to} item={item} />;
+    return <MenuItem label={label} to={to} item={item} />;
   }
 };
 
 const mapStateToProps = (state, ownProps) => {
+  let { location } = ownProps;
+
+  let locationParts = location.pathname.split("/");
+  let projectPermission = null;
+  if (locationParts[1] === "project") {
+    let projectId = state.getInPath(`entities.projects.byId.${locationParts[2]}.uid`);
+    projectPermission = getProjectPermissions(state, projectId);
+  }
+
   return {
     items: state.getInPath(`ui.navbar.items`),
-    user: state.getInPath("auth.user")
+    user: state.getInPath("auth.user"),
+    permission: projectPermission
   };
 };
 
