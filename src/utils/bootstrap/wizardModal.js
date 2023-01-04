bcfd5acdfb2f5f11c9dbde8564e64d8a16bc9672
muhammaddevops <66257016+muhammaddevops@users.noreply.github.com>
2023-01-04 23:32:45 +0000

    wizardModal made scrollable + close button

--- src/utils/bootstrap/wizardModal.js
+++ src/utils/bootstrap/wizardModal.js
@@ -1,17 +1,38 @@
 import React from "react";
+import { ErrorBoundary } from "traec-react/errors/handleError";
 
 export default function WizardModal(props) {
-  let { id, heading, subheading, body, hideClose } = props;
+  let { id, heading, body, hideClose } = props;
 
   return (
-    <div className="modal fade" id={id} tabIndex="-1" role="dialog" aria-labelledby={`${id}Label`} aria-hidden="true">
+    <div
+      className="modal fade"
+      id={id}
+      tabIndex="-1"
+      role="dialog"
+      aria-labelledby={`${id}Label`}
+      aria-hidden="true"
+      style={{ background: "white" }}
+    >
       <div
-        className="modal-dialog modal-dialog-centered mt-0 mb-0 "
+        className="modal-dialog modal-dialog-centered mt-0 mb-0"
         role="document"
         style={{ maxWidth: "none", width: "100%" }}
       >
-        <div className="modal-content modal-content-scrollable" style={{ height: "100vh" }}>
-          <div className={"modal-body p-0 bg-white container"}>{body}</div>
+        <div className="modal-content modal-content-scrollable border-0 d-flex" style={{ height: "100vh" }}>
+          <div className="modal-header border-0">
+            <div className="modal-title">{heading} </div>
+            {!hideClose ? (
+              <button type="button" className="close " data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true" className="">
+                  &times;
+                </span>
+              </button>
+            ) : null}
+          </div>
+          <ErrorBoundary>
+            <div className={"modal-body p-0 bg-white container"}>{body}</div>
+          </ErrorBoundary>
         </div>
       </div>
     </div>
